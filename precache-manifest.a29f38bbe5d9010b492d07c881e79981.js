3d115a49cd7c179aaf19db576aa37d169b33c6c0
Ozhey119 <ozhey119@gmail.com>
2020-08-27 01:22:43 +0300

    Updates

---
+++ precache-manifest.a29f38bbe5d9010b492d07c881e79981.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ea3e618f99007fb6c1fed4f4f3ee49",
+    "url": "/rallytrip/index.html"
+  },
+  {
+    "revision": "a975311c191e25bfc054",
+    "url": "/rallytrip/static/css/main.df77a612.chunk.css"
+  },
+  {
+    "revision": "e10a13830320fb7c69bb",
+    "url": "/rallytrip/static/js/2.ff2c9ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rallytrip/static/js/2.ff2c9ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a975311c191e25bfc054",
+    "url": "/rallytrip/static/js/main.0f3c1c64.chunk.js"
+  },
+  {
+    "revision": "0b9f78640a8948d4625b",
+    "url": "/rallytrip/static/js/runtime-main.709930db.js"
+  },
+  {
+    "revision": "975fb89a89e800d271a79cfcdc8dd8dc",
+    "url": "/rallytrip/static/media/1image one.975fb89a.jpg"
+  },
+  {
+    "revision": "2356a3172bbe131de4fd93fa8f6e0277",
+    "url": "/rallytrip/static/media/1image.2356a317.jpg"
+  },
+  {
+    "revision": "fd19b16ce1b7813cc50ef074723cc559",
+    "url": "/rallytrip/static/media/1image1.fd19b16c.jpg"
+  },
+  {
+    "revision": "f9b457b3cc7997692072b36f03e94552",
+    "url": "/rallytrip/static/media/2image.f9b457b3.jpg"
+  },
+  {
+    "revision": "6723675ae0a3c4176927c0aac533a07d",
+    "url": "/rallytrip/static/media/2image3.6723675a.jpg"
+  },
+  {
+    "revision": "c5cb996d79b458eb7e6cd6c3718665dc",
+    "url": "/rallytrip/static/media/3image.c5cb996d.jpg"
+  },
+  {
+    "revision": "43d114aee33441dd19bb169b315bb293",
+    "url": "/rallytrip/static/media/WhatsApp.43d114ae.png"
+  }
+]);
