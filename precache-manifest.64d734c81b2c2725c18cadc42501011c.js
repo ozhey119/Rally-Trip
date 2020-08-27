d9980a126226d6509ae5eaae7eedd8f0416ebb3b
Ozhey119 <ozhey119@gmail.com>
2020-08-27 12:38:08 +0300

    Updates

--- precache-manifest.64d734c81b2c2725c18cadc42501011c.js
+++ precache-manifest.64d734c81b2c2725c18cadc42501011c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c6b1ed9a1fa4bf512b482a6e3c95d3ca",
+    "revision": "edd17f834d235ab9fb484afb75c8ee07",
     "url": "/rallytrip/index.html"
   },
   {
-    "revision": "92c887de28191781f15d",
+    "revision": "b4c6d17fcd58c21e94ab",
     "url": "/rallytrip/static/css/main.df77a612.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rallytrip/static/js/2.fd66cf29.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92c887de28191781f15d",
-    "url": "/rallytrip/static/js/main.23425634.chunk.js"
+    "revision": "b4c6d17fcd58c21e94ab",
+    "url": "/rallytrip/static/js/main.246241e8.chunk.js"
   },
   {
     "revision": "0b9f78640a8948d4625b",
