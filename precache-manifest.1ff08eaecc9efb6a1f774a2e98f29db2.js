4d06c8d1472139d7676ec9a05472c2d10fef56b2
Ozhey119 <ozhey119@gmail.com>
2020-09-20 00:33:35 +0300

    Updates

---
+++ precache-manifest.1ff08eaecc9efb6a1f774a2e98f29db2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553ea6070151de09b8761110aef9389b",
+    "url": "/www.rallytrip.com/index.html"
+  },
+  {
+    "revision": "2b4be4184f9eb22d0d37",
+    "url": "/www.rallytrip.com/static/css/main.fcfcbda9.chunk.css"
+  },
+  {
+    "revision": "70e4ff2833b59257b96a",
+    "url": "/www.rallytrip.com/static/js/2.3f49ef19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www.rallytrip.com/static/js/2.3f49ef19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4be4184f9eb22d0d37",
+    "url": "/www.rallytrip.com/static/js/main.9f911892.chunk.js"
+  },
+  {
+    "revision": "dd776de9ba4279851bc8",
+    "url": "/www.rallytrip.com/static/js/runtime-main.61dab8ee.js"
+  },
+  {
+    "revision": "a8fc949e399abdf197068c30da98110f",
+    "url": "/www.rallytrip.com/static/media/10.a8fc949e.jpg"
+  },
+  {
+    "revision": "c54287fc8561330c81667204709f5bce",
+    "url": "/www.rallytrip.com/static/media/11.c54287fc.jpg"
+  },
+  {
+    "revision": "a981a899063840b4886036dba276712a",
+    "url": "/www.rallytrip.com/static/media/20.a981a899.jpg"
+  },
+  {
+    "revision": "2a8e437147661d24d6cb1d60df45ea89",
+    "url": "/www.rallytrip.com/static/media/21.2a8e4371.jpg"
+  },
+  {
+    "revision": "4b515eb1162c03e32535d9534776bd11",
+    "url": "/www.rallytrip.com/static/media/30.4b515eb1.jpg"
+  },
+  {
+    "revision": "9751db6b2865c89f19d8ccf824bc9c38",
+    "url": "/www.rallytrip.com/static/media/31.9751db6b.jpg"
+  },
+  {
+    "revision": "7af44d25aeb21b0e92e3d2b5848fc058",
+    "url": "/www.rallytrip.com/static/media/32.7af44d25.jpg"
+  },
+  {
+    "revision": "cb4ed73543cf0241304f2442a819a4e7",
+    "url": "/www.rallytrip.com/static/media/IMG-20200523-WA0008.cb4ed735.jpg"
+  },
+  {
+    "revision": "43d114aee33441dd19bb169b315bb293",
+    "url": "/www.rallytrip.com/static/media/WhatsApp.43d114ae.png"
+  },
+  {
+    "revision": "90e4d25ea49a7164d615692ce23a01a6",
+    "url": "/www.rallytrip.com/static/media/image.90e4d25e.jpg"
+  },
+  {
+    "revision": "cb1de287be0a15b440472b487b0a6b3a",
+    "url": "/www.rallytrip.com/static/media/image1.cb1de287.jpg"
+  },
+  {
+    "revision": "73dcd0d02f6f8060d0883305972ba77d",
+    "url": "/www.rallytrip.com/static/media/image2.73dcd0d0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RallyTrip/precache-manifest.0c074aa7d2a8527424cc708a688f68a7.js"
+  "/www.rallytrip.com/precache-manifest.1ff08eaecc9efb6a1f774a2e98f29db2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RallyTrip/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.rallytrip.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
