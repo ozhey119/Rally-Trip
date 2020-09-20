cc95d1def69c04ff787a716147e6480ae81bcf05
Ozhey119 <ozhey119@gmail.com>
2020-09-20 13:52:45 +0300

    Updates

---
+++ precache-manifest.1ba432513d8396b8b977de31f312c0a4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b65bfccb9a91b5b16a0357558c504c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb22edc7b6db837ca435",
+    "url": "/static/css/main.fcfcbda9.chunk.css"
+  },
+  {
+    "revision": "70e4ff2833b59257b96a",
+    "url": "/static/js/2.3f49ef19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f49ef19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb22edc7b6db837ca435",
+    "url": "/static/js/main.b2cf3948.chunk.js"
+  },
+  {
+    "revision": "2d666a71dd463d3a1456",
+    "url": "/static/js/runtime-main.4af83a02.js"
+  },
+  {
+    "revision": "a8fc949e399abdf197068c30da98110f",
+    "url": "/static/media/10.a8fc949e.jpg"
+  },
+  {
+    "revision": "c54287fc8561330c81667204709f5bce",
+    "url": "/static/media/11.c54287fc.jpg"
+  },
+  {
+    "revision": "a981a899063840b4886036dba276712a",
+    "url": "/static/media/20.a981a899.jpg"
+  },
+  {
+    "revision": "2a8e437147661d24d6cb1d60df45ea89",
+    "url": "/static/media/21.2a8e4371.jpg"
+  },
+  {
+    "revision": "4b515eb1162c03e32535d9534776bd11",
+    "url": "/static/media/30.4b515eb1.jpg"
+  },
+  {
+    "revision": "9751db6b2865c89f19d8ccf824bc9c38",
+    "url": "/static/media/31.9751db6b.jpg"
+  },
+  {
+    "revision": "7af44d25aeb21b0e92e3d2b5848fc058",
+    "url": "/static/media/32.7af44d25.jpg"
+  },
+  {
+    "revision": "cb4ed73543cf0241304f2442a819a4e7",
+    "url": "/static/media/IMG-20200523-WA0008.cb4ed735.jpg"
+  },
+  {
+    "revision": "43d114aee33441dd19bb169b315bb293",
+    "url": "/static/media/WhatsApp.43d114ae.png"
+  },
+  {
+    "revision": "90e4d25ea49a7164d615692ce23a01a6",
+    "url": "/static/media/image.90e4d25e.jpg"
+  },
+  {
+    "revision": "cb1de287be0a15b440472b487b0a6b3a",
+    "url": "/static/media/image1.cb1de287.jpg"
+  },
+  {
+    "revision": "73dcd0d02f6f8060d0883305972ba77d",
+    "url": "/static/media/image2.73dcd0d0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/www.rallytrip.com/precache-manifest.1ff08eaecc9efb6a1f774a2e98f29db2.js"
+  "/precache-manifest.1ba432513d8396b8b977de31f312c0a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.rallytrip.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
